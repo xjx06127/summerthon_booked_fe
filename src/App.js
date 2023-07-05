e5326ed9429f8cd16c2139cbdf0c19cbc71e4ce6
hyori Choi <hyoring030@gmail.com>
2023-07-05 23:57:16 +0900

    라우팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import Write from "./pages/Write";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-<h1>화이또</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element = {<h1>화이또</h1>}/>
+      <Route path="/:WriteBook" element = {<Write />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from "styled-components";
+
+
+const BoxEach = styled.div`
+
+  margin: 15px;
+  border-radius: 5px;
+  display: inline-block;
+  background-color:rgba(217, 217, 217, 1);
+  width: 350px;
+  height: 380px;
+
+`;
+
 
 const Write = () => {
     return (
         <div>
-            
+            <BoxEach></BoxEach>
         </div>
     );
 };
